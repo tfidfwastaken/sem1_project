9082cd2156e5a8cce34a21190848961eb7a665d6
Atharva Raykar <connectatharvaraykar@gmail.com>
2018-11-06 17:06:19 +0530

    Scoop-di-Whoop

---
+++ README.md
@@ -0,0 +1,2 @@
+# Time-Tablify
+Creates Time Tables
---
+++ timetable.py
@@ -0,0 +1,6 @@
+"""
+Creates a time tables based on the following inputs:
+Number of classes
+Number of subjects and their credits
+Other stuff like let-off days etc.
+"""
