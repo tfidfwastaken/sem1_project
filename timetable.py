24898e82dc9216a5a9c3081cad5af776ee5b3570
Atharva Raykar <connectatharvaraykar@gmail.com>
2018-11-07 00:13:59 +0530

    Update class and cleanup

--- timetable.py
+++ timetable.py
@@ -7,44 +7,34 @@ Other stuff like let-off days etc.
 """
 
 class Timetable:
-    def __init__(self, slots, subj_dict):
-        self._slots = slots
-        # subj_dict is a dictionary with subjects as key and credits as value
-        self._subj_dict = subj_dict
+    def __init__(self, tt_dict):
+        self._tt_dict = tt_dict
     
-    def get_slots(self):
-        return self._slots
+    def get_tt_dict(self):
+        return self._tt_dict
     
-    def get_subj_dict(self):
-        return self._subj_dict
-    
-    slots = property(get_slots)
-    subj_dict = property(get_subj_dict)
+    tt_dict = property(get_tt_dict)
     
     """
     def generate_day_sched():
-        Does this to the subj_dict for example:
+        Does this to the subj_dict for example the sub-dict in tt_dict:
         {"Maths": 2, "EC": 1, "CS": 1} --> ["Maths", "EC", "Maths", "CS"]
         We just need to make it a list having each subject *frequency* number
         of times and shuffle its contents
     """
 
-# We need a function that takes a subj_dict for a whole week (week_dict)
-# and makes them into 7 subj_dict's for each day
-# slot_dict contains slots mapped to each day
-#   like {"Mon": 6, "Tue": 6, "Wed": 4, "Thu": 6, "Fri": 5}
-
+# Takes the quota of subjects per week and slots per week
+# and makes a time table in dict form
 def dist_subjects(week_dict, slot_dict):
     tt_dict = {}     # Eg: {"Mon":{"s1":1, "s2":2}, "Tue":{"s1":2, "s3":3}, ...}
-    #for day in slot_dict:
-        #subj_dict_by_day[day] = {}
     if sum(week_dict.values()) > sum(slot_dict.values()):
         hr_deficit = sum(week_dict.values()) - sum(slot_dict.values())
         print("Too many hours to accomodate.")
         print(f"Please reduce {hr_deficit} hours from the week or increase those many slots")
     else:
-        for subject in sorted(week_dict, key=week_dict.__getitem__, reverse=True):
-            for day in sorted(slot_dict, key=slot_dict.__getitem__, reverse=True):
+        # Just making tt_dict's keys and initializing to empty/zero
+        for subject in week_dict:
+            for day in slot_dict:
                     if day not in tt_dict:
                         tt_dict[day] = {}
                     if subject not in tt_dict[day]:
@@ -60,14 +50,18 @@ def dist_subjects(week_dict, slot_dict):
             subj_quota[subj] -= 1
     return tt_dict
 
-# Test: Example time tables
-TT1 = Timetable(4, {"Maths": 2, "EC": 1, "CS": 1})
-TT2 = Timetable(6, {"Maths": 2, "EC": 1, "CS": 2, "Bio": 1})
-
-#print(TT1.slots)
-#print(TT2.subj_dict)
-
 # Test: the distribution algorithm
-print(dist_subjects(
+tt1 = dist_subjects(
     {"Maths": 5, "Chem": 4, "EC": 4, "Bio": 2, "Comp": 4, "CSLab": 2, "ChemLab": 2},
-    {"Mon": 6, "Tue": 6, "Wed": 4, "Thu": 6, "Fri": 5}))
+    {"Mon": 6, "Tue": 6, "Wed": 4, "Thu": 6, "Fri": 5})
+
+tt2 = dist_subjects(
+    {"Cooking": 3, "Carpentry": 5, "Pole-Vaulting": 4},
+    {"Mon": 4, "Wed": 5, "Fri": 3})
+
+# Test: Example time tables
+TT1 = Timetable(tt1)
+TT2 = Timetable(tt2)
+
+print(TT1.tt_dict)
+print(TT2.tt_dict)
