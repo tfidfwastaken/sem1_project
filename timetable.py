976cf250fcedeb4f6b74bfec1a1659dd551843f1
Atharva Raykar <connectatharvaraykar@gmail.com>
2018-11-06 17:41:39 +0530

    Hours -> Slots

--- timetable.py
+++ timetable.py
@@ -1,29 +1,31 @@
 """
 Creates a time tables based on the following inputs:
-Number of hours
+Number of slots
 Number of classes
 Number of subjects and their credits
 Other stuff like let-off days etc.
 """
 
 class Timetable:
-    def __init__(self, hours, subj_dict):
-        self._hours = hours
+    def __init__(self, slots, subj_dict):
+        self._slots = slots
         # subj_dict is a dictionary with subjects as key and credits as value
         self._subj_dict = subj_dict
     
-    def get_hours(self):
-        return self._hours
+    def get_slots(self):
+        return self._slots
     
     def get_subj_dict(self):
         return self._subj_dict
     
-    hours = property(get_hours)
+    slots = property(get_slots)
     subj_dict = property(get_subj_dict)
+    
+    
 
 # Test: Example time tables
 TT1 = Timetable(4, {"Maths": 2, "EC": 1, "CS": 1})
 TT2 = Timetable(6, {"Maths": 2, "EC": 1, "CS": 2, "Bio": 1})
 
-print(TT1.hours)
+print(TT1.slots)
 print(TT2.subj_dict)
