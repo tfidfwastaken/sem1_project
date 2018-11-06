ddf130cca9472023a754fddea7967dd57af0e3b1
Atharva Raykar <connectatharvaraykar@gmail.com>
2018-11-06 18:27:45 +0530

    Document more on design

--- timetable.py
+++ timetable.py
@@ -21,10 +21,25 @@ class Timetable:
     slots = property(get_slots)
     subj_dict = property(get_subj_dict)
     
+    """
+    def generate_day_sched():
+        Does this to the subj_dict for example:
+        {"Maths": 2, "EC": 1, "CS": 1} --> ["Maths", "EC", "Maths", "CS"]
+        We just need to make it a list having each subject *frequency* number
+        of times and shuffle its contents
+    """
 
-# We need a function that takes a subj_dict for a whole week
+# We need a function that takes a subj_dict for a whole week (week_dict)
 # and makes them into 7 subj_dict's for each day
-# def dist_subjects(...)
+# slot_dict contains slots mapped to each day
+def dist_subjects(week_dict, slot_dict):
+    subj_dict_list = []     # Eg: [{"s1":1, "s2":2}, {"s1":2, "s3":3}, ...]
+    if sum(week_dict.values()) > sum(slot_dict.values()):
+        hr_deficit = sum(week_dict.values()) - sum(slot_dict.values())
+        print("Too many hours to accomodate.")
+        print(f"Please reduce {hr_deficit} hours from the week or increase those many slots")
+    # else: distribute the slots of each subject as evenly as possible
+        
 
 # Test: Example time tables
 TT1 = Timetable(4, {"Maths": 2, "EC": 1, "CS": 1})
